860029e4bf06427b78a721513e2b3b81402507e9
luvwitch <itsalexkelly@gmail.com>
2020-12-07 15:36:29 -0800

    inquirer prompts added

--- company.js
+++ company.js
@@ -1,8 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-var PORT = process.env.PORT || 8080;
-  
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -14,6 +12,75 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    //runSearch(); < replace with starter function that includes inquirer prompts
-    //reference mysql activity 14 if confused
+    start();
   });
+
+  function start() {
+    inquirer
+      .prompt({
+        name: "q1",
+        type: "list",
+        message: "Would you like to ADD, VIEW, or UPDATE a department, role, or employee?",
+        choices: ["ADD", "VIEW", "UPDATE"]
+
+        }).then(function(answer) {
+          if (answer.q1 === "ADD") {
+            addBegin();
+          } else if(answer.q1 === "VIEW") {
+            viewBegin();
+          } else if(answer.q1 === "UPDATE") {
+            updateBegin();            
+          } else{
+            connection.end();
+          }
+        });
+  }
+
+  function addBegin(){
+    inquirer
+    .prompt({
+      name: "qAdd",
+      type: "list",
+      message: "Would you like to add a department, role, or employee?",
+      choices: ["DEPARTMENT", "ROLE", "EMPLOYEE"]
+
+      }).then(function(answer) {
+        if (answer.qAdd === "DEPARTMENT") {
+          addDepartment();
+        } else if(answer.qAdd === "ROLE") {
+          addRole();
+        } else if(answer.qAdd === "EMPLOYEE") {
+          addEmployee();            
+        } else{
+          connection.end();
+        }
+      });
+  }
+
+  function viewBegin(){
+    inquirer
+    .prompt({
+      name: "qView",
+      type: "list",
+      message: "Would you like to view a department, role, employee, or all?",
+      choices: ["DEPARTMENT", "ROLE", "EMPLOYEE", "ALL"]
+
+      }).then(function(answer) {
+        if (answer.qView === "DEPARTMENT") {
+          viewDepartment();
+        } else if(answer.qView === "ROLE") {
+          viewRole();
+        } else if(answer.qView === "EMPLOYEE") {
+          viewEmployee(); 
+        } else if(answer.qView === "ALL") {
+          viewAll();             
+        } else{
+          connection.end();
+        }
+      });
+
+  }
+
+  function updateBegin(){
+
+  }
